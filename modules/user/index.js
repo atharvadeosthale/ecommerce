497b7e2013142c2adfebf8f46b32439b816c3954
Atharva Deosthale <atharva.deosthale17@gmail.com>
2020-06-20 02:52:37 +0530

    Created Login and Register API

--- modules/user/index.js
+++ modules/user/index.js
@@ -1,10 +1,62 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
 
-router.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(401).json({ msg: "Email and Password are required" });
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(401).json({ msg: "Email and Password are required" });
+    }
+    const user = await User.findOne({ email, password });
+    if (!user) {
+      return res.status(401).json({ msg: "Incorrect email or password" });
+    }
+    const token = await jwt.sign({ email }, process.env.SECRET_AUTH_KEY);
+    user.token = token;
+    const { name, mobile, cart } = user;
+    user.save();
+    res.json({ email, name, mobile, token, cart });
+  } catch (err) {
+    console.error(err);
+    res.status(501).json({ msg: "The server encountered an error" });
+  }
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, mobile } = req.body;
+    if (!name || !email || !password || !mobile) {
+      return res.status(400).json({ msg: "All the fields are required" });
+    }
+    const checkEmail = await User.findOne({ email });
+    const checkMobile = await User.findOne({ mobile });
+    if (checkEmail) {
+      // user exists
+      return res
+        .status(400)
+        .json({ msg: "User with that email already exists" });
+    }
+    if (checkMobile) {
+      // user exists
+      return res
+        .status(400)
+        .json({ msg: "User with that mobile already exists" });
+    }
+    const token = await jwt.sign({ email }, process.env.SECRET_AUTH_KEY);
+    const newUser = new User({
+      name,
+      email,
+      password,
+      mobile,
+      token,
+    });
+    newUser.save();
+    res.json({ name, email, mobile, token, cart: [] });
+  } catch (err) {
+    console.error(err);
+    res.status(501).json({ msg: "The server encountered an error" });
   }
 });
 
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 const db = require("./connections/db");
 const auth = require("./modules/auth");
 const user = require("./modules/user");
 const PORT = 3000;
 
+dotenv.config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/user", user);
